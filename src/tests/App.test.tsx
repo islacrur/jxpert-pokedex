aaf3819e825b34c5f28c4b694684d37a5d7d4001
isla.cruanes <islacrur@gmail.com>
2025-05-23 17:58:07 +0200

    config: tests

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,7 @@
+import {  describe, expect, test } from 'vitest';
+
+describe('App Component', () => {
+  test('debería verse el nombre del pokemon cuando se cargan los datos', async () => {
+   expect(true).toBe(true)
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgrPlugin from "vite-plugin-svgr";
+import { configDefaults } from 'vitest/config';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgrPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.ts'],
+    exclude: [
+      ...configDefaults.exclude,
+    ],
+    include: [
+      'src/**/*.test.ts',
+      'src/**/*.test.tsx'
+    ]
+  },
 });
