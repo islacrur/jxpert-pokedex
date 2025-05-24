0d15e302617e0b1e139caa9c24c572002b1d149f
isla.cruanes <islacrur@gmail.com>
2025-05-24 12:47:02 +0200

    Se configura ts y tsx

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ import tsparser from "@typescript-eslint/parser";
 export default [
   { ignores: ["dist"] },
   {
-    files: ["/*.{js,jsx}"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
