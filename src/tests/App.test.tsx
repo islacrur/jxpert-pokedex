6a6b176fabb6208bc7f0d16f904b935dbbcb7535
isla.cruanes <islacrur@gmail.com>
2025-05-24 10:18:05 +0200

    test: falsoo

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -99,6 +99,6 @@ describe('App Component', () => {
     await userEvent.type(placeholder, 'ivysaur')
   //expect(screen.getByText(/bulbasaur/i)).toBeInTheDocument();
     //  expect(screen.getByText(/ivysaur/i)).toBeInTheDocument();  });
-    expect(false).toBe(true)});
+    expect(true).toBe(false)});
 //not to be in document
 });
