deb989f096c771a033cb570f11210b6ac276e6a3
isla.cruanes <islacrur@gmail.com>
2025-05-30 22:39:43 +0200

    feat: se añade el test que filtra por la región de Unova

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -196,6 +196,29 @@ describe('App Component', () => {
     )
   })
 
+  it('debería filtrar por la región Unova', async () => {
+    const mockFetch = vi.fn()
+    globalThis.fetch = mockFetch
+    setupMockFetch([{ id: 1, name: 'bulbasaur' }], mockFetch)
+    setupMockFetch([{ id: 3, name: 'charizard' }], mockFetch)
+
+    render(<App />)
+
+    expect(await screen.findByText('bulbasaur')).toBeVisible()
+
+    const combobox = screen.getByRole('combobox', { name: /select reg/i })
+    await userEvent.click(combobox)
+
+    const opcion = await screen.findByText(/unova/i)
+    await userEvent.click(opcion)
+
+    expect(await screen.findByText('charizard')).toBeVisible()
+    expect(globalThis.fetch).toHaveBeenNthCalledWith(
+      3,
+      `https://pokeapi.co/api/v2/pokemon?offset=494&limit=649`,
+    )
+  })
+
   it('debería filtrar por la región de Alola', async () => {
     const mockFetch = vi.fn()
     globalThis.fetch = mockFetch
