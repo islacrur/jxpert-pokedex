34c70c91ee621808201775c80b6d4d7b20af0e69
isla.cruanes <islacrur@gmail.com>
2025-05-30 22:51:01 +0200

    feat: se añade el test que ordena las cartas según los puntos de velocidad

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -379,4 +379,17 @@ describe('App Component', () => {
 
     expect(ivysaur.compareDocumentPosition(bulbasaur)).toBe(4)
   })
+
+  it('debería ordenar las cartas por puntos de velocidad', async () => {
+    render(<App />)
+
+    const combobox = screen.getByRole('combobox', { name: /sort by/i })
+    await userEvent.click(combobox)
+    const option = screen.getAllByLabelText('Speed')
+    await userEvent.click(option[0])
+    const bulbasaur = screen.getByText('bulbasaur')
+    const ivysaur = screen.getByText('ivysaur')
+
+    expect(ivysaur.compareDocumentPosition(bulbasaur)).toBe(4)
+  })
 })
