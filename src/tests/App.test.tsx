dedbcf51c57e3c59b829e8e8fda0e450423118f8
isla.cruanes <islacrur@gmail.com>
2025-05-30 22:45:51 +0200

    feat: se añade el test que ordena las cartas según los puntos de defensa

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -340,4 +340,17 @@ describe('App Component', () => {
 
     expect(ivysaur.compareDocumentPosition(bulbasaur)).toBe(4)
   })
+
+  it('debería ordenar las cartas por puntos de defensa', async () => {
+    render(<App />)
+
+    const combobox = screen.getByRole('combobox', { name: /sort by/i })
+    await userEvent.click(combobox)
+    const option = screen.getAllByLabelText('Defense')
+    await userEvent.click(option[0])
+    const bulbasaur = screen.getByText('bulbasaur')
+    const ivysaur = screen.getByText('ivysaur')
+
+    expect(ivysaur.compareDocumentPosition(bulbasaur)).toBe(4)
+  })
 })
