767c0b5157fcd87343b8dfad57fbe42a0b16d86f
isla.cruanes <islacrur@gmail.com>
2025-05-30 22:43:58 +0200

    feat: se añade el test que ordena las cartas según los puntos de ataque

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -327,4 +327,17 @@ describe('App Component', () => {
 
     expect(ivysaur.compareDocumentPosition(bulbasaur)).toBe(4)
   })
+
+  it('debería ordenar las cartas por puntos de ataque', async () => {
+    render(<App />)
+
+    const combobox = screen.getByRole('combobox', { name: /sort by/i })
+    await userEvent.click(combobox)
+    const option = screen.getAllByLabelText('Attack')
+    await userEvent.click(option[0])
+    const bulbasaur = screen.getByText('bulbasaur')
+    const ivysaur = screen.getByText('ivysaur')
+
+    expect(ivysaur.compareDocumentPosition(bulbasaur)).toBe(4)
+  })
 })
