d6a808d58da0bd43dafc631b62ba23a45c4381b8
isla.cruanes <islacrur@gmail.com>
2025-06-06 19:59:52 +0200

    feat: se crea CardContent

---
+++ src/components/CardContent.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { CriteriaProgressBar } from './CriteriaProgressBar'
+type CardContentProps = {
+  title: string
+  stats: any
+}
+
+export const CardContent: React.FC<CardContentProps> = ({ title, stats }) => {
+  return (
+    <section className="card__content">
+      <h3 className="card__title">{title}</h3>
+      <ul aria-description="Stats resume">
+        <CriteriaProgressBar statName="Hp" statValue={stats[0].base_stat} />
+        <CriteriaProgressBar statName="At" statValue={stats[1].base_stat} />
+        <CriteriaProgressBar statName="Df" statValue={stats[2].base_stat} />
+        <CriteriaProgressBar statName="SpA" statValue={stats[3].base_stat} />
+        <CriteriaProgressBar statName="SpD" statValue={stats[4].base_stat} />
+        <CriteriaProgressBar statName="Spd" statValue={stats[5].base_stat} />
+      </ul>
+    </section>
+  )
+}
