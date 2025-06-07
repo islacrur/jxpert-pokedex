cdb29f12ea08a474189303507926fd46c3ceb1af
dayana <dayana.ruiz@inetum.com>
2025-06-07 13:41:11 +0200

    feat: create service Pokemons

---
+++ src/core/services/pokemonsService.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import{REGIONS, Region, Pokemon, PokemonsList, CRITERIA, Criteria} from '../../appTypes'
+
+
+const getAllPokemons = async (region: Region) => {
+  const activeRegion = REGIONS[region]
+  const regionStart = activeRegion.regionStart
+  const regionEnd = activeRegion.regionEnd
+
+  const { results }: PokemonsList = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?offset=${regionStart}&limit=${regionEnd}`,
+  ).then((response) => response.json())
+
+  const result: Pokemon[] = await Promise.all(
+    results.map(
+      async ({ url }) => await fetch(url).then((response) => response.json()),
+    ),
+  )
+  return result
+}
+
+export const pokemonsService = {
+    getAllPokemons
+}
