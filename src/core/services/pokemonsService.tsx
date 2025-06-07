affe0ce6fd87e19aaba79b8d0fb64cc9301d38e4
dayana <dayana.ruiz@inetum.com>
2025-06-07 13:46:45 +0200

    fix: se cambia constante por función

--- src/core/services/pokemonsService.tsx
+++ src/core/services/pokemonsService.tsx
@@ -1,18 +1,18 @@
 import{REGIONS, Region, Pokemon, PokemonsList} from '../../appTypes'
 
-const getAllPokemons = async (region: Region) => {
+async function getAllPokemons(region: Region) {
   const activeRegion = REGIONS[region]
   const regionStart = activeRegion.regionStart
   const regionEnd = activeRegion.regionEnd
 
   const { results }: PokemonsList = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?offset=${regionStart}&limit=${regionEnd}`,
+    `https://pokeapi.co/api/v2/pokemon?offset=${regionStart}&limit=${regionEnd}`
   ).then((response) => response.json())
 
   const result: Pokemon[] = await Promise.all(
     results.map(
-      async ({ url }) => await fetch(url).then((response) => response.json()),
-    ),
+      async ({ url }) => await fetch(url).then((response) => response.json())
+    )
   )
   return result
 }
