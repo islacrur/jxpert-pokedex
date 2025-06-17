14fde9b4735ce471c862d49e0a945c41a0d66323
isla.cruanes <islacrur@gmail.com>
2025-06-17 18:08:25 +0200

    refactor: se completa la clase pokemon y stat

--- src/contextos/core/pokemon/domain/Pokemon.ts
+++ src/contextos/core/pokemon/domain/Pokemon.ts
@@ -1,6 +1,11 @@
+import { Stat } from './Stat'
+import { Type } from './Type'
+
 export class Pokemon {
   constructor(
     private _id: number,
     private _name: string,
+    private _types: Type[],
+    private _stats: Stat[],
   ) {}
 }
--- src/contextos/core/pokemon/domain/Stat.ts
+++ src/contextos/core/pokemon/domain/Stat.ts
@@ -2,5 +2,7 @@ export class Stat {
   constructor(
     private _name: string,
     private _url: string,
+    private _base_stat: number,
+    private _effort: number,
   ) {}
 }
