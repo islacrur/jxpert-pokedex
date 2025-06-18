a51d4dd149274eb64a21f9aee4c5e201b0c28355
dayana <dayana.ruiz@inetum.com>
2025-06-18 18:59:58 +0200

    refactor: se modifica el getAllPokemonsByRegion para que devuelva un array de Pokemon

--- src/contextos/core/pokemon/infrastructure/ApiPokemonRepository.ts
+++ src/contextos/core/pokemon/infrastructure/ApiPokemonRepository.ts
@@ -2,7 +2,7 @@
 // import { PokemonRepository } from '../domain/PokemonRepository'
 // import { PokemonList } from '../domain/PokemonList'
 // import { Pokemon } from '../domain/Pokemon'
-// import { PokemonDTO } from '../domain/PokemonDTO'
+import { PokemonDTO } from '../domain/PokemonDTO'
 
 // export class ApiPokemonRepository implements PokemonRepository {
 //   async getAllPokemonsByRegion(region: Region): Promise<Pokemon[]> {
@@ -43,19 +43,51 @@ import { Pokemon } from '../domain/Pokemon'
 
 export class ApiPokemonRepository implements PokemonRepository {
   async getAllPokemonsByRegion(region: Region): Promise<Pokemon[]> {
-    const activeRegion = REGIONS[region]
-    const regionStart = activeRegion.regionStart
-    const regionEnd = activeRegion.regionEnd
+    const activeRegion = REGIONS[region];
+    const regionStart = activeRegion.regionStart;
+    const regionEnd = activeRegion.regionEnd;
 
     const { results }: PokemonList = await fetch(
-      `https://pokeapi.co/api/v2/pokemon?offset=${regionStart}&limit=${regionEnd}`,
-    ).then((response) => response.json())
+      `https://pokeapi.co/api/v2/pokemon?offset=${regionStart}&limit=${regionEnd}`
+    ).then((response) => response.json());
 
-    const result: Pokemon[] = await Promise.all(
-      results.map(
-        async ({ url }) => await fetch(url).then((response) => response.json()),
-      ),
-    )
-    return result
+    const pokemonsDTO: PokemonDTO[] = await Promise.all(
+      results.map(async ({ url }) => {
+        const data = await fetch(url).then((res) => res.json());
+
+        const dto: PokemonDTO = {
+          id: data.id,
+          name: data.name,
+          artwork: data.sprites.other['official-artwork'].front_default,
+          types: data.types.map((t: any) => t.type.name),
+          stats: data.stats.map((s: any) => ({
+            name: s.stat.name,
+            base: s.base_stat,
+            effort: s.effort,
+            url: s.stat.url,
+          })),
+        };
+
+        return dto;
+      })
+    );
+
+    return pokemonsDTO.map((pokemonDTO) => this.mapPokemonDTOToPokemon(pokemonDTO));
   }
+
+ mapPokemonDTOToPokemon(pokemonDTO: PokemonDTO): Pokemon {
+  return {
+    id: pokemonDTO.id,
+    name: pokemonDTO.name,
+    imageUrl: pokemonDTO.artwork,
+    primarytype: pokemonDTO.types[0] || '',
+    secondarytype: pokemonDTO.types[1] || '',
+    stats: pokemonDTO.stats.map(stat => ({
+      name: stat.name,
+      baseValue: stat.base,
+      effort: stat.effort,
+      url: stat.url,
+    })),
+  };
+}
 }
