b8448a1afd87f8ff243d78fdbff17e67e66144ca
dayana <dayana.ruiz@inetum.com>
2025-06-18 18:48:38 +0200

    refactor: modificación de la intefaz PokemonDTO que donde se mapea los datos recibidos del fetch

--- src/contextos/core/pokemon/domain/PokemonDTO.ts
+++ src/contextos/core/pokemon/domain/PokemonDTO.ts
@@ -1,27 +1,12 @@
-export class PokemonDTO {
-  id: number
-  name: string
-  type: string
-  level: number
-  hp: number
-  attack: number
-  defense: number
-
-  constructor(
-    id: number,
-    name: string,
-    type: string,
-    level: number,
-    hp: number,
-    attack: number,
-    defense: number,
-  ) {
-    this.id = id
-    this.name = name
-    this.type = type
-    this.level = level
-    this.hp = hp
-    this.attack = attack
-    this.defense = defense
-  }
+export interface PokemonDTO {
+  id:number;
+  name: string;
+  artwork: string;
+  types: string[];
+  stats: {
+    name: string;
+    base: number;
+    effort: number;
+    url: string;
+  }[];
 }
